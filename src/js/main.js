4ec686f936738004c1cd0eaa73efd3eed43d26fb
DimanYX <f5f3e920f1eef1e8@gmail.com>
2024-07-12 11:30:09 +0300

    add action for header catalog menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,13 @@ headerCatalogBtn.addEventListener("click" , (event)=>{
     headerCatalogBody.classList.toggle("header-catalog-wrapper_active")
     document.querySelector("body").classList.toggle("body_active")
 })
+
+
+document.querySelector("body").addEventListener('click', (event)=>{
+    if(event.target.classList.value == "body_active"){
+        headerCatalogBtn.classList.toggle("header-catalog__btn_active")
+        headerCatalogBody.classList.toggle("header-catalog-wrapper_active")
+        document.querySelector("body").classList.toggle("body_active")
+    }
+})
+
